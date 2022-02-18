0aff5c43a9a0ae5ac4fae9461a9400205f69b159
anshumalivfx <anshumali.karna99@gmail.com>
2022-02-18 22:51:21 +0530

    Added CCP

--- app/src/main/java/com/anshumali/evgeo/SignInPage.java
+++ app/src/main/java/com/anshumali/evgeo/SignInPage.java
@@ -8,10 +8,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.hbb20.CountryCodePicker;
+
 public class SignInPage extends AppCompatActivity {
 
-    private EditText phoneNumber;
+    private EditText phoneNumber, otpBlock;
     private Button signinButton;
+    private CountryCodePicker ccp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +24,32 @@ public class SignInPage extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in_page);
         phoneNumber = findViewById(R.id.phoneNumber);
         signinButton = findViewById(R.id.signin);
-
+        ccp = findViewById(R.id.countryCode_picker);
+        otpBlock = findViewById(R.id.otpBlock);
         signinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (phoneNumber.getText().toString().matches("[0-9]+")){
+                    otpBlock.setVisibility(View.VISIBLE);
+                    phoneNumber.setFocusable(false);
+                    ccp.registerCarrierNumberEditText(phoneNumber);
+                    signinButton.setText("SignIn");
+                    signinButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Toast.makeText(getApplicationContext(), "SignIn" + ccp.getFormattedFullNumber(), Toast.LENGTH_SHORT).show();
 
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "FUCK YOU BRO", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    public void signIn(){
+
     }
 }
