abfca7ec7485fe4f2d2f3d60bb022f1035e2fa77
anshumalivfx <anshumali.karna99@gmail.com>
2022-02-18 21:11:06 +0530

    Firebase Init

--- app/src/main/java/com/anshumali/evgeo/SignInPage.java
+++ app/src/main/java/com/anshumali/evgeo/SignInPage.java
@@ -3,12 +3,28 @@ package com.anshumali.evgeo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignInPage extends AppCompatActivity {
+
+    private EditText phoneNumber;
+    private Button signinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_page);
+        phoneNumber = findViewById(R.id.phoneNumber);
+        signinButton = findViewById(R.id.signin);
+
+        signinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
